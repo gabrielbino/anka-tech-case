22961de93b57e022fc38ee2dfe44e76be471dc23
gabrielbino <gabriellf650@gmail.com>
2025-06-03 00:43:06 -0300

    chore: registra novas rotas com estrutura modularizada

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,13 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { clientRoutes } from './routes/clients'
+import { clientRoutes } from './routes/clientRoutes'
 import { assetRoutes } from './routes/assets'
 import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-app.register(clientRoutes)
+app.register(clientRoutes, { prefix: '/clients' })
 app.register(assetRoutes)
 
 app.listen({ port: 3001 }, () => {
